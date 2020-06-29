a5b9534d184df2c93a5a3aa4910fe583cb8aa515
Mahnatse-rgb <mfchoung01@gmail.com>
2020-06-29 15:39:04 +0200

    refactor code

--- Searching_algorithms/linear_search.py
+++ Searching_algorithms/linear_search.py
@@ -4,12 +4,14 @@ array = [7,9,4,7,3,56,78,94,10,44]
 target =  7
 
 print(f"In an arrary of {array} :")
-
+found = False
 def linear_sort(array, target):
     for i in range(len(array)):
         if array[i] == target:
+            found = True
             print(f"{target} is on position {i}")
-    else:
+
+    if not found:
         print(f"{target} not found")
 
-linear_sort(array,target)
+linear_sort(array, target)
